bc5bfff81f53df07250d03f9f9c5c7a301dcb4b1
ARROKO <84386958+ARROKO@users.noreply.github.com>
2025-12-01 17:13:20 +0100

    fix(debug): add ErrorBoundary to catch runtime crashes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import ErrorBoundary from './components/ErrorBoundary'
+
+console.log('Main.tsx executing...');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
